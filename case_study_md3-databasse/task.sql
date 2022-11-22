0ab0aed8b421d1e99c35f485dcdd576c333897bd
congtranc0722g1 <trantancongall@gmail.com>
2022-11-22 11:00:11 +0700

    nop

--- case_study_md3-databasse/task.sql
+++ case_study_md3-databasse/task.sql
@@ -173,3 +173,174 @@ from
   left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong 
 group by 
   hop_dong.ma_hop_dong;
+  
+  -- Task 11:
+  select 
+  dich_vu_di_kem.ma_dich_vu_di_kem, 
+  dich_vu_di_kem.ten_dich_vu_di_kem 
+from 
+  dich_vu_di_kem 
+  join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem 
+  join hop_dong on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong 
+  join khach_hang on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang 
+  join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach 
+where 
+  loai_khach.ten_loai_khach like "Diamond" 
+  and (
+    khach_hang.dia_chi like "% Vinh" 
+    or khach_hang.dia_chi like "% Quảng Ngãi"
+  ) 
+group by 
+  dich_vu_di_kem.ma_dich_vu_di_kem;
+  
+  -- Task 12:
+  select 
+  hop_dong.ma_hop_dong, 
+  nhan_vien.ho_ten, 
+  khach_hang.ho_ten, 
+  khach_hang.so_dien_thoai, 
+  dich_vu.ten_dich_vu, 
+  ifnull(
+    sum(hop_dong_chi_tiet.so_luong), 
+    0
+  ) as so_luong_dich_vu_di_kem, 
+  hop_dong.tien_dat_coc 
+from 
+  hop_dong 
+  right join nhan_vien on hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien 
+  right join khach_hang on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang 
+  join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu 
+  left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong 
+where 
+  hop_dong.ma_hop_dong in (
+    select 
+      hop_dong.ma_hop_dong 
+    from 
+      hop_dong 
+    where 
+      year(hop_dong.ngay_lam_hop_dong) = 2020 
+      and quarter(hop_dong.ngay_lam_hop_dong) = 4
+  ) 
+  and hop_dong.ngay_lam_hop_dong not in (
+    select 
+      hop_dong.ngay_lam_hop_dong 
+    from 
+      hop_dong 
+    where 
+      year(hop_dong.ngay_lam_hop_dong) = 2021 
+      and quarter(hop_dong.ngay_lam_hop_dong) in (1, 2)
+  ) 
+group by 
+  hop_dong.ma_hop_dong;
+  
+  -- Task 13: 
+  create view v_dich_vu_di_kem as 
+select 
+  dich_vu_di_kem.ma_dich_vu_di_kem, 
+  dich_vu_di_kem.ten_dich_vu_di_kem, 
+  ifnull(
+    sum(hop_dong_chi_tiet.so_luong), 
+    0
+  ) as so_luong_dich_vu_di_kem 
+from 
+  dich_vu_di_kem 
+  join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem 
+  join hop_dong on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong 
+group by 
+  dich_vu_di_kem.ma_dich_vu_di_kem;
+  
+  select 
+  * 
+from 
+  v_dich_vu_di_kem 
+where 
+  so_luong_dich_vu_di_kem = (
+    select 
+      max(so_luong_dich_vu_di_kem) 
+    from 
+      v_dich_vu_di_kem
+  );
+  
+  -- Task 14: 
+  select 
+  hop_dong.ma_hop_dong, 
+  loai_dich_vu.ten_loai_dich_vu, 
+  dich_vu_di_kem.ten_dich_vu_di_kem, 
+  count(
+    hop_dong_chi_tiet.ma_dich_vu_di_kem
+  ) as so_lan_su_dung 
+from 
+  hop_dong 
+  join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu 
+  join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu 
+  join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong 
+  join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem 
+group by 
+  hop_dong_chi_tiet.ma_dich_vu_di_kem 
+having 
+  count(
+    hop_dong_chi_tiet.ma_dich_vu_di_kem
+  ) = 1 
+order by 
+  hop_dong.ma_hop_dong;
+  
+  -- Task 15:
+  select 
+  nhan_vien.ma_nhan_vien, 
+  nhan_vien.ho_ten, 
+  trinh_do.ten_trinh_do, 
+  bo_phan.ten_bo_phan, 
+  nhan_vien.so_dien_thoai, 
+  nhan_vien.dia_chi 
+from 
+  nhan_vien 
+  join trinh_do on nhan_vien.ma_trinh_do = trinh_do.ma_trinh_do 
+  join bo_phan on nhan_vien.ma_bo_phan = bo_phan.ma_bo_phan 
+  join hop_dong on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien 
+where 
+  year(hop_dong.ngay_lam_hop_dong) in (2020, 2021) 
+group by 
+  nhan_vien.ma_nhan_vien 
+having 
+  count(hop_dong.ma_nhan_vien) <= 3 
+order by 
+  nhan_vien.ma_nhan_vien;
+  
+  -- Task 16:
+  set sql_safe_updates = 0;
+  
+delete from 
+  nhan_vien 
+where 
+  nhan_vien.ma_nhan_vien in (
+    select 
+      * 
+    from 
+      (
+        select 
+          nhan_vien.ma_nhan_vien 
+        from 
+          nhan_vien 
+        where 
+          nhan_vien.ma_nhan_vien not in (
+            select 
+              nhan_vien.ma_nhan_vien 
+            from 
+              nhan_vien 
+              join hop_dong on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien 
+            where 
+              hop_dong.ma_nhan_vien in (
+                select 
+                  hop_dong.ma_nhan_vien 
+                where 
+                  year(hop_dong.ngay_lam_hop_dong) in (2019, 2020, 2021)
+              ) 
+            group by 
+              nhan_vien.ma_nhan_vien
+          )
+      ) adc
+  );
+  
+  -- Task 17: 
+  
+  
