07d4920a8b877c3c24ae5b8ae114ac3c39466a0e
congtranc0722g1 <trantancongall@gmail.com>
2022-11-23 16:53:05 +0700

    alo

---
+++ case_study_furama_v2/furama_v2.sql
@@ -0,0 +1,48 @@
+create database furama_v2;
+use furama_v2;
+
+create table customer_type(
+id int primary key,
+name varchar(45)
+);
+
+create table customer(
+id int primary key,
+customer_type_id int,
+`name` varchar(45),
+date_of_birth date,
+gender bit(1),
+id_card varchar(45),
+phone_number varchar(45),
+email varchar(45),
+address varchar(45),
+foreign key(customer_type_id) references customer_type(id)
+);
+
+create table facility_type (
+id int primary key,
+`name` varchar(45)
+);
+
+create table rent_type(
+id int primary key,
+`name` varchar(45)
+);
+
+create table facility(
+id int primary key,
+`name` varchar(45),
+area int,
+cost double,
+max_people int,
+rent_type_id int,
+facility_type_id int,
+standard_room varchar(45),
+description_other_convenience varchar(45),
+pool_area double,
+numbers_of_floors int,
+facility_free text,
+foreign key(rent_type_id) references rent_type(id),
+foreign key(facility_type_id) references facility_type(id)
+);
+
